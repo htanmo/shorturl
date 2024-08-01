05f646c3ace3aad8214cb7018948e4ea2ae2687a
htanmo <145841395+htanmo@users.noreply.github.com>
2024-08-02 02:16:32 +0530

    added: forwarding and handlers

--- main.go
+++ main.go
@@ -4,15 +4,23 @@ import (
 	"fmt"
 
 	"github.com/gin-gonic/gin"
+	"github.com/htanmo/shorturl/handler"
+	"github.com/htanmo/shorturl/store"
 )
 
 func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "Hey Go URL Shortener !",
+			"message": "Welcome to the URL Shortener API",
 		})
 	})
+
+	r.POST("/create-short-url", handler.CreateShortUrl)
+
+	r.GET("/:shortUrl", handler.HandleShortUrlRedirect)
+
+	store.InitializeStore()
 
 	err := r.Run(":9808")
 	if err != nil {
